a9f102a2101be28e345338bfda65d0da75106b17
Keshan J <keshanth64@gmail.com>
2019-09-03 19:41:18 +0530

    First commit

--- voice_recognition.py
+++ voice_recognition.py
@@ -7,10 +7,42 @@ from playsound import playsound
 from scipy.io.wavfile import write
 
 
-fs = 44100  # Sample rate
-seconds = 3  # Duration of recording
 r = sr.Recognizer()  # obtain source from the microphone
 audio_path = 'audio/output.wav'
+commands = ['open chrome', 'stop', 'whats the time', 'chrome', 'google']
+
+
+def user_command(inp):
+    for command in commands:
+        if inp.lower() == command:
+            if inp.lower() == 'stop':
+                print('[-] Okay bye')
+            else:
+                print(command)
+                time.sleep(2)
+                record()
+        else:
+            time.sleep(2)
+            record()
+
+
+def speech_to_text(audio):
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("[+] You said : " + r.recognize_google(audio))
+        user_command(r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("[-] Sorry, could not understand audio")
+        time.sleep(2)
+        record()
+    except sr.RequestError as e:
+        print(
+            "[-] Could not request results from Google Speech Recognition service; {0}".format(e))
+        time.sleep(2)
+        record()
 
 
 def speak(text):
@@ -26,31 +58,24 @@ def listen(src):
         return r.record(source)
 
 
-def speech_to_text(audio):
-    text = r.recognize_google(audio)
-    # recognize speech using Google Speech Recognition
+def record():
+    fs = 44100  # Sample rate
+    seconds = 3  # Duration of recording
     try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("You said : " + text)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print(
-            "Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Listening: ")
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        sd.wait()  # Wait until recording is finished
+        write(audio_path, fs, myrecording)  # Save as WAV file
+        # Extract audio data and sampling rate from file
+        data, fs = sf.read(audio_path)
+        # Save as FLAC file at correct sampling rate
+        sf.write(audio_path, data, fs)
+
+        src = sr.AudioFile(audio_path)  # obtain source from the audio
+        audio = listen(src)
+        speech_to_text(audio)
+    except KeyboardInterrupt:
+        print('[-] You stopped the program')
 
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write(audio_path, fs, myrecording)  # Save as WAV file
-# Extract audio data and sampling rate from file
-data, fs = sf.read(audio_path)
-# Save as FLAC file at correct sampling rate
-sf.write(audio_path, data, fs)
-
-src = sr.AudioFile(audio_path)  # obtain source from the audio
-audio = listen(src)
-speech_to_text(audio)
-
-
+record()
