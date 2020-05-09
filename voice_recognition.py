46412f869340b546edcf1d5049b388373bc5ed59
keshan-spec <keshanth.programmer@gmail.com>
2020-05-09 22:08:00 +0530

    Fixed issues

--- voice_recognition.py
+++ voice_recognition.py
@@ -11,30 +11,6 @@ class SpeechRecognition():
     def __init__(self):
         self.mic = sr.Microphone()
         self.r = sr.Recognizer()  # obtain source from the microphone
-        self.commands = ['open chrome', 'stop',
-                         'whats the time', 'chrome', 'google', 'hello']
-
-    def speak(self, text):
-        filename = "speak/audio.mp3"
-        tts = gTTS(text)
-        tts.save(filename)
-        playsound(filename)
-
-    # gets the speech and checks for any command
-    def user_command(self, inp):
-        for command in self.commands:
-            if inp.lower() == command:
-                if inp.lower() == 'hello':
-                    self.speak('Okay bye')
-                else:
-                    print(command)
-                    audio = self.listen()
-                    self.speech_to_text(audio)
-            else:
-                print("[-] No command")
-                time.sleep(2)
-                audio = self.listen()
-                self.speech_to_text(audio)
 
     def speech_to_text(self, audio):
         # recognize speech using Google Speech Recognition
@@ -43,53 +19,61 @@ class SpeechRecognition():
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
             print("[+] You said : " + self.r.recognize_google(audio))
-            self.user_command(self.r.recognize_google(audio))
+            self.listen()
         except sr.UnknownValueError:
             print("[-] Sorry, could not understand audio")
-            time.sleep(2)
-            audio = self.listen()
-            self.speech_to_text(audio)
+            time.sleep(1)
+            self.listen()
         except sr.RequestError as e:
             print(
                 "[-] Could not request results from Google Speech Recognition service; {0}".format(e))
-            time.sleep(2)
-            audio = self.listen()
-            self.speech_to_text(audio)
+            time.sleep(1)
+            self.listen()
         except KeyboardInterrupt:
             print("[-] You stopped the program")
 
     # listens for audio from the mic
     # Speech recognition module
     def listen(self):
-        with self.mic as source:
-            print("Listening...")
-            self.r.adjust_for_ambient_noise(source)
-            return self.r.listen(source)
+        try:
+            with self.mic as source:
+                print("Listening...")
+                self.r.adjust_for_ambient_noise(source)
+                src = self.r.listen(source)
+            self.speech_to_text(src)
+        except KeyboardInterrupt:
+            print('[-] You stopped the program')
+            return
+        except AssertionError:
+            print('[-] Error')
 
     # records the audio and saves it to a .wav file
-    # sounddevice module
+    # sound device module
     def record(self):
         fs = 44100  # Sample rate
         seconds = 3  # Duration of recording
-        audio_path = 'audio/output.wav'  # audio path to save
+        audio_path = 'output.wav'  # audio path to save
         try:
             print("Listening: ")
             myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
             sd.wait()  # Wait until recording is finished
             write(audio_path, fs, myrecording)  # Save as WAV file
-            # Extract audio data and sampling rate from file
-            data, fs = sf.read(audio_path)
-            # Save as FLAC file at correct sampling rate
-            sf.write(audio_path, data, fs)
-
+            data, fs = sf.read(audio_path)  # Extract audio data and sampling rate from file
+            sf.write(audio_path, data, fs)  # Save as FLAC file at correct sampling rate
             src = sr.AudioFile(audio_path)  # obtain source from the audio
-            audio = self.listen()
-            self.speech_to_text(audio)
+            self.speech_to_text(src)
         except KeyboardInterrupt:
             print('[-] You stopped the program')
+            return
+
+    @staticmethod
+    def speak(text):
+        filename = "speak/audio.mp3"
+        tts = gTTS(text)
+        tts.save(filename)
+        playsound(filename)
 
 
 # speech class obj
 speech = SpeechRecognition()
-audio = speech.listen()
-speech.speech_to_text(audio)
+speech.listen()
